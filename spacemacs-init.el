1051abb940619d7c3fb53bc4329bc149b47df86f
Marcel Becker <marcelbecker@gmail.com>
2017-06-19 17:57:31 -0700

    Added swipper to init.

--- spacemacs-init.el
+++ spacemacs-init.el
@@ -2,7 +2,7 @@
 
 (setq user-emacs-directory (expand-file-name "~/Dropbox/.spacemacs.d/"))
 (add-to-list 'load-path user-emacs-directory)
-(setq package-user-dir (concat user-emacs-directory "/elpa"))
+(setq package-user-dir (concat user-emacs-directory "elpa"))
 (add-to-list 'load-path package-user-dir)
 
 (load (concat user-emacs-directory "init.el"))
